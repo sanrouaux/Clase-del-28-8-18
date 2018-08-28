cf8de394ccce406c60b865f26214d8939854c04a
sanrouaux <sanrouaux@gmail.com>
2018-08-28 18:32:41 -0300

    Cuatro funciones listas

--- TP Calculadora/main.c
+++ TP Calculadora/main.c
@@ -1,44 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int ingresarOpcion(void);
+int sumar(int, int);
+int restar(int, int);
+int dividir(int, int);
+int multiplicar(int, int);
+int sacarFactorial(int, int);
+
 
 int main()
 {
     int opcion;
     int numeroUno;
     int numeroDos;
+    int resultado;
 
     do
     {
-        printf("1. Sumar \n");
-        printf("2. Restar \n");
-        printf("3. Multiplicar \n");
-        printf("4. Dividir \n");
-        printf("5. Salir \n");
-        printf("Ingrese una opción: \n");
+        printf("Ingrese el primer numero: ");
+        scanf("%d", &numeroUno);
 
-        opcion = ingresarOpcion();
+        printf("\nIngrese el segundo numero: ");
+        scanf("%d", &numeroDos);
+
+        printf("\nQue accion desea ralizar?");
+        printf("\n1. Calcular la suma de %d y %d", numeroUno, numeroDos);
+        printf("\n2. Calcular la resta de %d y %d", numeroUno, numeroDos);
+        printf("\n3. Calcular la division entre %d y %d", numeroUno, numeroDos);
+        printf("\n4. Calcular la multiplicacion entre %d y %d", numeroUno, numeroDos);
+        printf("\n5. Calcular el factorial de %d y de %d", numeroUno, numeroDos);
+        printf("\nIngrese una opcion: ");
+        scanf("%d", &opcion);
 
 
         switch(opcion)
         {
         case 1:
-            printf("Estoy sumando");
-            numeroUno = ingresarOpcion();
-            numeroDos = ingresarOpcion();
+            resultado = sumar(numeroUno, numeroDos);
+            printf("\nEl resultado de %d+%d es: %d\n", numeroUno, numeroDos, resultado);
             break;
 
         case 2:
-            printf("Estoy restando");
+            resultado = restar(numeroUno, numeroDos);
+            printf("\nEl resultado de %d-%d es: %d\n", numeroUno, numeroDos, resultado);
             break;
 
         case 3:
-            printf("Estoy multiplicando");
+            if(numeroDos == 0){
+                printf("\nNo es posible dividir por cero\n");
+            } else {
+                resultado = dividir(numeroUno, numeroDos);
+                printf("\nEl resultado de %d/%d es: %d\n", numeroUno, numeroDos, resultado);
+            }
             break;
 
         case 4:
-            printf("Estoy dividiendo");
+            resultado = multiplicar(numeroUno, numeroDos);
+            printf("\nEl resultado de %d*%d es: %d\n", numeroUno, numeroDos, resultado);
             break;
 
         case 5:
@@ -53,16 +71,39 @@ int main()
         system("cls");
 
     }
-    while(opcion != 5)
+    while(opcion != 5);
 
     return 0;
 }
 
-int ingresarOpcion(void){
-    int opcion
-    printf("Ingrese un numero");
-    scanf("%d", opcion);
 
-    return opcion;
+int sumar(int numeroUno, int numeroDos)
+{
+    int resultado;
+    resultado = numeroUno + numeroDos;
+    return resultado;
 }
 
+int restar(int numeroUno, int numeroDos)
+{
+    int resultado;
+    resultado = numeroUno - numeroDos;
+    return resultado;
+}
+
+int dividir(int numeroUno, int numeroDos)
+{
+    float resultado;
+    resultado = numeroUno / numeroDos;
+    return resultado;
+}
+
+int multiplicar(int numeroUno, int numeroDos)
+{
+
+}
+
+int sacarFactorial(int numeroUno, int numeroDos)
+{
+
+}
