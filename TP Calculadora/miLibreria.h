38ee5a56e6d05f0a11c32f50771e60b6b8abf278
sanrouaux <sanrouaux@gmail.com>
2018-09-02 16:02:57 -0300

    Calculadora mas miLibreria

---
+++ TP Calculadora/miLibreria.h
@@ -0,0 +1,8 @@
+int ingresarNumero (void);
+int desplegarMenu (int, int);
+int sumar(int, int);
+int restar(int, int);
+float dividir(int, int);
+int multiplicar(int, int);
+int sacarFactorial(int);
+void imprimirResultado (float);
